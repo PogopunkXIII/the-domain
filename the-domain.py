0f45c57542c4e61eb7b61e29375a334ff2114f00
PogopunkXIII <pogopunkxiii@gmail.com>
2020-01-21 21:24:26 +0000

    added two different get methods

--- the-domain.py
+++ the-domain.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 domain = Flask(__name__)
 
@@ -19,7 +19,18 @@ tasks = [
 
 @domain.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-	return jsonifty({'tasks': tasks})
+	return jsonify({'tasks': tasks})
+
+@domain.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@domain.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	domain.run(debug=True)
