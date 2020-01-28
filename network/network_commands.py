a98d5606f03a538ffff1d2b2a9f0df9e64dc4409
PogopunkXIII <pogopunkxiii@gmail.com>
2020-01-28 00:57:50 +0000

    added a dumb method for tracking computer names to mac addresses

--- network/network_commands.py
+++ network/network_commands.py
@@ -1,5 +1,7 @@
 import subprocess
 
+computers = { "Installation04", "04-D4-C4-54-D3-E9" }
+
 def wake_on_lan(computer_name):
     #need to get the mac address of my computer, stored in a dictionary?
-    wol = subprocess.run(["./commands/wol.sh", mac_address_to_wake])
+    wol = subprocess.run(["./commands/wol.sh", computers[computer_namecomputers[computer_name])
